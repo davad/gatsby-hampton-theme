792436e782b1c10c13eb139bad54ac4d474cecca
David Landry <david@dmwl.net>
2018-01-25 20:43:51 -0500

    Add global 'Home' button

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { injectGlobal } from 'react-emotion';
+import { injectGlobal, css } from 'react-emotion';
 import PropTypes from 'prop-types';
+import { TiHome } from 'react-icons/lib/ti';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import { Link } from '../components/Misc';
 
 /* eslint-disable */
 injectGlobal`
@@ -19,9 +21,23 @@ injectGlobal`
 `;
 /* eslint-enable */
 
+const homeStyle = css`
+  box-shadow: none;
+  font-size: 2rem;
+  z-index: 1000;
+  position: absolute;
+  left: 72px;
+  top: 16px;
+`;
+
 const Layout = ({ children }) => (
   <div>
     <Navigation />
+    <div>
+      <Link to="/" className={homeStyle}>
+        <TiHome />
+      </Link>
+    </div>
     {children()}
     <Footer />
   </div>

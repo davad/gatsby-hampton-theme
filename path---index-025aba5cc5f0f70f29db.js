89256b17f00281c1ac13773876efa1d89d331544
David Landry <david@dmwl.net>
2018-01-25 20:45:11 -0500

    Updates

--- path---index-025aba5cc5f0f70f29db.js
+++ path---index-025aba5cc5f0f70f29db.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{344:function(t,e){t.exports={data:{allContentJson:{edges:[{node:{index:{title:"Hello World",subtitle:"This is a simple gatsby-starter"}}}]}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{345:function(t,e){t.exports={data:{allContentJson:{edges:[{node:{index:{title:"Hello World",subtitle:"This is a simple gatsby-starter"}}}]}},pathContext:{}}}});
 //# sourceMappingURL=path---index-025aba5cc5f0f70f29db.js.map
--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/gatsby-hampton-theme/app-164838005ef7dca7e858.js","a349225bc32c7f05ac0912b20f028a63"],["/gatsby-hampton-theme/app-56a0dc7561b9540cc80b.js","083a9fdc75fe16789c8bde05537d5783"],["/gatsby-hampton-theme/app-be464d2d91dd628a58eb.js","10d2f14311ac51d45642d90b39ba8d51"],["/gatsby-hampton-theme/commons-6fd2234b7c0021dc05f9.js","8e80eb5e2abe83a67e30518adbf43ea8"],["/gatsby-hampton-theme/index.html","9944a25d5bd0ca106d69e4ceec2ea74d"],["/gatsby-hampton-theme/offline-plugin-app-shell-fallback/index.html","950a419275bd9f6ad2da9979679bed1a"]];
+var precacheConfig = [["/gatsby-hampton-theme/app-164838005ef7dca7e858.js","a349225bc32c7f05ac0912b20f028a63"],["/gatsby-hampton-theme/app-56a0dc7561b9540cc80b.js","0373e5255a3aedf9f4b084e1d5697306"],["/gatsby-hampton-theme/app-be464d2d91dd628a58eb.js","10d2f14311ac51d45642d90b39ba8d51"],["/gatsby-hampton-theme/commons-6fd2234b7c0021dc05f9.js","17900714e5d0e0c613bd78f07a8648a9"],["/gatsby-hampton-theme/index.html","7fc8a3a150786359967b1d194f3e8a20"],["/gatsby-hampton-theme/offline-plugin-app-shell-fallback/index.html","7219e2320e14b99f6fdbd173ede594a3"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 

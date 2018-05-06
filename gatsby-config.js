32383c820ec8bf0575ac6c6bcf34aec6940225b1
Max Beizer <max.beizer@gmail.com>
2018-05-05 20:14:24 -0500

    Fix linting error in gatsby-config
    The comma in the gatsby config violates the `prettier` rules

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: 'src/utils/typography.js',
+        pathToConfigModule: 'src/utils/typography.js'
       }
     }
   ]
